e3247a748286db25c2dbac48c8ffb9afb499e149
MASTERS <anusara.orm@gmail.com>
2018-03-21 16:25:30 +0700

    Initial commit

---
+++ app/src/main/java/com/minewbeacon/blescan/demo/addname/AddActivity.java
@@ -0,0 +1,15 @@
+package com.minewbeacon.blescan.demo.addname;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yuliwuli.blescan.demo.R;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
